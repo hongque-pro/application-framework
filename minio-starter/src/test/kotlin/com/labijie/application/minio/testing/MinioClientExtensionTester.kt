b545107413d2e290ca40134579dc552e8e132d05
endink <sharping@outlook.com>
2021-05-12 22:42:12 +0800

    fix unit test

--- minio-starter/src/test/kotlin/com/labijie/application/minio/testing/MinioClientExtensionTester.kt
+++ minio-starter/src/test/kotlin/com/labijie/application/minio/testing/MinioClientExtensionTester.kt
@@ -2,7 +2,10 @@ package com.labijie.application.minio.testing
 
 import com.labijie.application.BucketPolicy
 import com.labijie.appliction.minio.makeBucketIfNotExisted
+import io.minio.MinioClient
+import io.minio.RemoveBucketArgs
 import java.net.ConnectException
+import java.net.SocketTimeoutException
 import kotlin.test.Test
 
 class MinioClientExtensionTester {
@@ -14,6 +17,20 @@ class MinioClientExtensionTester {
             client.makeBucketIfNotExisted("test", BucketPolicy.PUBLIC)
             client.makeBucketIfNotExisted("test", BucketPolicy.PUBLIC)
         }catch (e: ConnectException){
+            e.printStackTrace()
+        }
+        catch (e:SocketTimeoutException){
+            e.printStackTrace()
+        }
+        finally {
+            deleteBucket(client, "test")
+        }
+    }
+
+    private fun deleteBucket(client: MinioClient, bucket:String){
+        try {
+            client.removeBucket(RemoveBucketArgs.builder().bucket(bucket).build())
+        }catch (e: Exception){
             e.printStackTrace()
         }
     }
--- minio-starter/src/test/kotlin/com/labijie/application/minio/testing/MinioTestHelper.kt
+++ minio-starter/src/test/kotlin/com/labijie/application/minio/testing/MinioTestHelper.kt
@@ -5,24 +5,28 @@ import com.labijie.appliction.minio.configuration.MinioProperties
 import io.minio.MinioClient
 import okhttp3.OkHttpClient
 import java.net.URL
+import java.time.Duration
 
 object MinioTestHelper {
     private val minioProperties = MinioProperties(
-        endpoint = URL("http://47.100.188.60:9001"),
+        endpoint = URL("http://47.100.188.60:9000"),
         accessKey = "stsadmin",
         secretKey = "stsadmin-secret"
     )
+
+    private val httpClient =  OkHttpClient.Builder().connectTimeout(Duration.ofSeconds(1)).build()
 
     private const val testApplicationName = "testing-app"
 
 
     fun createMinioClient(): MinioClient {
         return MinioClient.builder()
+            .httpClient(httpClient)
             .endpoint(minioProperties.endpoint)
             .credentials(minioProperties.accessKey, minioProperties.secretKey).build()
     }
 
     fun createMinioUtils(): MinioUtils{
-        return MinioUtils(testApplicationName, minioProperties, OkHttpClient())
+        return MinioUtils(testApplicationName, minioProperties, httpClient)
     }
 }

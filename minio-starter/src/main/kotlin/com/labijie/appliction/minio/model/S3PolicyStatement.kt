ffaed00793a6fb51c0071a23a57b20f6bbd1c4de
AndersXiao <sharping@outlook.com>
2021-05-13 19:34:19 +0800

    fox package name

--- minio-starter/src/main/kotlin/com/labijie/appliction/minio/model/S3PolicyStatement.kt
+++ minio-starter/src/main/kotlin/com/labijie/appliction/minio/model/S3PolicyStatement.kt
@@ -3,8 +3,8 @@ package com.labijie.appliction.minio.model
 import com.fasterxml.jackson.annotation.JsonProperty
 
 open class S3PolicyStatement(buckets: List<String>) {
-    @get:JsonProperty("Sid")
-    var sid: String = "Stmt1"
+//    @get:JsonProperty("Sid")
+//    var sid: String = "Stmt1"
 
     @get:JsonProperty("Effect")
     var effect: String = S3Policy.POLICY_EFFECT_ALLOW

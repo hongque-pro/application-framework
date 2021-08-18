b2165277d1c38e0c770f71e60fe784487267b9e2
Victor <huhongwei@gznb.com>
2021-08-18 12:25:06 +0800

    关闭订单

--- payment-starter/src/main/kotlin/com/labijie/application/payment/PaymentExchangeUrls.kt
+++ payment-starter/src/main/kotlin/com/labijie/application/payment/PaymentExchangeUrls.kt
@@ -1,11 +1,11 @@
 package com.labijie.application.payment
 
 open class PaymentExchangeUrls(
-    var createTradeUrl:String = "https://api.mch.weixin.qq.com/pay/unifiedorder",
-    var queryTradeUrl: String = "https://api.mch.weixin.qq.com/pay/orderquery",
-    var transferUrl:String = "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers",
-    var queryTransferUrl:String = "https://api.mch.weixin.qq.com/mmpaymkttransfers/gettransferinfo",
-    var refundUrl:String = "https://api.mch.weixin.qq.com/secapi/pay/refund",
-    var queryRefundUrl:String = "https://api.mch.weixin.qq.com/pay/refundquery",
-    var closeTradeUrl:String = "https://api.mch.weixin.qq.com/pay/closeorder"
+        var createTradeUrl:String = "https://api.mch.weixin.qq.com/pay/unifiedorder",
+        var queryTradeUrl: String = "https://api.mch.weixin.qq.com/pay/orderquery",
+        var transferUrl:String = "https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers",
+        var queryTransferUrl:String = "https://api.mch.weixin.qq.com/mmpaymkttransfers/gettransferinfo",
+        var refundUrl:String = "https://api.mch.weixin.qq.com/secapi/pay/refund",
+        var queryRefundUrl:String = "https://api.mch.weixin.qq.com/pay/refundquery",
+        var closeTradeUrl:String = "https://api.mch.weixin.qq.com/pay/closeorder"
 )

802f467db49bb29d1bb2815d12463da089409f30
endink <sharping@outlook.com>
2023-12-24 22:12:29 +0800

    change touch file

--- core-web-starter/src/main/kotlin/com/labijie/application/web/controller/FileController.kt
+++ core-web-starter/src/main/kotlin/com/labijie/application/web/controller/FileController.kt
@@ -9,6 +9,8 @@ import com.labijie.application.model.FileModifier
 import com.labijie.application.model.ObjectPreSignUrl
 import com.labijie.application.service.IFileIndexService
 import com.labijie.application.service.TouchedFile
+import com.labijie.infra.utils.ifNullOrBlank
+import org.hibernate.validator.constraints.Length
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 
 @RestController
@@ -38,11 +41,16 @@ class FileController : ApplicationContextAware {
 
     @PostMapping("/touch")
     fun touch(
-        @RequestParam("filePath", required = true) filePath: String,
+        @RequestParam("folder", required = true) @Length(min = 1, max = 128) folder: String,
+        @RequestParam("ext", required = false) fileExtensions: String?,
         @RequestParam("modifier", required = true) modifier: FileModifier
     ): TouchedFile {
 
-        return fileIndexService.touchFile(filePath, modifier)
+        val name = UUID.randomUUID().toString().replace("-", "").lowercase()
+        val ext = fileExtensions.orEmpty().trimStart('.')
+        val normalizedFolder = folder.trim('/')
+        val fullPath = "${normalizedFolder}/${name}.${ext}"
+        return fileIndexService.touchFile(fullPath, modifier)
     }
 
     override fun setApplicationContext(applicationContext: ApplicationContext) {

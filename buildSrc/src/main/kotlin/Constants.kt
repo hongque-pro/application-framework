1ea672e15e4ec9960ddb4fd3517470895f767338
endink <sharping@outlook.com>
2024-01-01 17:22:20 +0800

    upgrade caching kotlin

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -19,7 +19,7 @@ object Versions {
     const val infraOrm = "2.0.0"
     const val infraPlugin = "2.0.2"
     const val infraCommons = "3.0.0"
-    const val infraCaching = "1.4.0"
+    const val infraCaching = "1.4.1"
     const val infraOAuth2 = "2.0.0"
     const val infraCloudStream = "1.2.1"
 

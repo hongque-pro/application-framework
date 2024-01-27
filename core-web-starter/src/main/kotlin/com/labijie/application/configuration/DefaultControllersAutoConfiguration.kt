63e9f0fd69a9a9b8b04e854b72f2d3c24bdd3023
endink <sharping@outlook.com>
2024-01-27 22:22:46 +0800

    fix controller

--- core-web-starter/src/main/kotlin/com/labijie/application/configuration/DefaultControllersAutoConfiguration.kt
+++ core-web-starter/src/main/kotlin/com/labijie/application/configuration/DefaultControllersAutoConfiguration.kt
@@ -12,6 +12,7 @@ import com.labijie.application.web.controller.FileController
 import com.labijie.application.web.controller.SmsController
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.context.annotation.Bean
@@ -25,11 +26,13 @@ import org.springframework.context.annotation.Import
 @ConditionalOnProperty(prefix = "application.web.service-controllers", name = ["enabled"], havingValue = "true", matchIfMissing = true)
 class DefaultControllersAutoConfiguration {
     @Bean
+    @ConditionalOnMissingBean(ApplicationController::class)
     fun applicationController(): ApplicationController {
         return ApplicationController()
     }
 
     @Bean
+    @ConditionalOnMissingBean(SmsController::class)
     fun smsController(
         userService: IUserService,
         messageService: IMessageService
@@ -38,6 +41,7 @@ class DefaultControllersAutoConfiguration {
     }
 
     @Bean
+    @ConditionalOnMissingBean(FileController::class)
     fun fileController(fileIndexService: IFileIndexService): FileController {
         return FileController(fileIndexService)
     }

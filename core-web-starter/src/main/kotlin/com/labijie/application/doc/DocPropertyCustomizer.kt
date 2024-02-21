9bf34bd2caa69451fef6fd2e1e3887d7f38ba13a
endink <sharping@outlook.com>
2024-02-21 12:18:26 +0800

    fix spring doc when use normal json mode

--- core-web-starter/src/main/kotlin/com/labijie/application/doc/DocPropertyCustomizer.kt
+++ core-web-starter/src/main/kotlin/com/labijie/application/doc/DocPropertyCustomizer.kt
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.JavaType
 import com.labijie.application.IDescribeEnum
 import com.labijie.application.JAVA_DOUBLE
 import com.labijie.application.JAVA_LONG
+import com.labijie.application.configuration.ApplicationWebProperties
+import com.labijie.application.configuration.JsonMode
 import io.swagger.v3.core.converter.AnnotatedType
 import io.swagger.v3.core.util.Json
 import io.swagger.v3.oas.models.media.IntegerSchema
 import io.swagger.v3.oas.models.media.Schema
 import io.swagger.v3.oas.models.media.StringSchema
 import org.springdoc.core.customizers.PropertyCustomizer
+import org.springframework.boot.autoconfigure.web.WebProperties
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
 
@@ -20,13 +23,13 @@ import java.math.BigDecimal
  */
 
 @Component
-class DocPropertyCustomizer : PropertyCustomizer {
+class DocPropertyCustomizer(private val webProperties: ApplicationWebProperties) : PropertyCustomizer {
 
     override fun customize(property: Schema<*>, type: AnnotatedType): Schema<*> {
         if (DocUtils.isEnumType(type.type)) {
             return DocUtils.getEnumSchema(property, type.type)
         }
-        if (DocUtils.isStringNumberType(type.type)) {
+        if (webProperties.jsonMode == JsonMode.JAVASCRIPT && DocUtils.isStringNumberType(type.type)) {
             return DocUtils.getNumberSchema(property, type.type)
         }
         return property

8ba5e1d5ff9d56b13905e23ea5a42136e522d298
endink <sharping@outlook.com>
2024-07-10 11:42:41 +0800

    recover cors

--- core-web-starter/src/main/kotlin/com/labijie/application/configuration/AllowAllCorsAutoConfiguration.kt
+++ core-web-starter/src/main/kotlin/com/labijie/application/configuration/AllowAllCorsAutoConfiguration.kt
@@ -1,7 +1,15 @@
 package com.labijie.application.configuration
 
+import org.springframework.boot.autoconfigure.AutoConfigureOrder
+import org.springframework.boot.autoconfigure.security.SecurityProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
@@ -14,6 +22,7 @@ import org.springframework.web.filter.CorsFilter
  */
 @Configuration(proxyBeanMethods = false)
 class AllowAllCorsAutoConfiguration {
+
     private fun buildConfig(): CorsConfiguration {
         val corsConfiguration = CorsConfiguration()
         corsConfiguration.addAllowedOriginPattern("*")
@@ -31,4 +40,5 @@ class AllowAllCorsAutoConfiguration {
         source.registerCorsConfiguration("/**", buildConfig())
         return CorsFilter(source)
     }
+
 }

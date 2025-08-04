e938814472a1ae717d7cdcf118451993920bea1e
endink <sharping@outlook.com>
2025-08-04 23:58:04 +0800

    fix hcaptcha

--- hcaptcha-starter/src/main/kotlin/com/labijie/application/hcaptcha/configuration/HCaptchaAutoConfiguration.kt
+++ hcaptcha-starter/src/main/kotlin/com/labijie/application/hcaptcha/configuration/HCaptchaAutoConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureBefore
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
 
@@ -19,6 +20,7 @@ import org.springframework.web.client.RestClient
 @EnableConfigurationProperties(HCaptchaProperties::class)
 class HCaptchaAutoConfiguration {
 
+    @Bean
     @ConditionalOnProperty(
         prefix = "application.captcha.hcaptcha",
         name = ["enabled"],
